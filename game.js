26772512f65a777927b7a56a4f9279415fb05a34
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-08-04 17:20:40 -0600

    Start and outline class

---
+++ game.js
@@ -0,0 +1,24 @@
+var Player = require(".player.js");
+
+class Game {
+  trackGameData() {
+
+  }
+
+  selectGameType() {
+
+  }
+
+  checkForWinConditions() {
+
+  }
+
+  detectDraw() {
+
+  }
+
+  resetBoard() {
+    
+  }
+
+}
