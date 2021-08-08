2c468a76ed8d3fb4fe39af047e6dc9de89d26dbd
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-08-08 14:09:56 -0600

    Add event listeners to fighter icons

---
+++ main.js
@@ -0,0 +1,7 @@
+var rockBtnClassic = document.querySelector(".rockBtnClassic");
+var paperBtnClassic = document.querySelector(".paperBtnClassic");
+var scissorsBtnClassic = document.querySelector(".scissorsBtnClassicJ");
+
+rockBtnClassic.addEventListener("click", takeTurn);
+paperBtnClassic = addEventListener("click", takeTurn);
+paperBtnClassic = addEventListener("click", takeTurn);
