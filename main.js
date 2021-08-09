b02414ee4daa86c596d41efcb319021660a2eaf5
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-08-09 11:22:55 -0600

    Add functionality to Choose Game Mode buttons

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-var rockBtnClassic = document.querySelector(".rockBtnClassic");
-var paperBtnClassic = document.querySelector(".paperBtnClassic");
-var scissorsBtnClassic = document.querySelector(".scissorsBtnClassicJ");
+var chooseYourGameView = document.querySelector(".choose-your-game-view");
+var classicModeView = document.querySelector(".classic-mode-view");
+var difficultModeView = document.querySelector(".difficult-mode-view");
 
-rockBtnClassic.addEventListener("click", takeTurn);
-paperBtnClassic = addEventListener("click", takeTurn);
-paperBtnClassic = addEventListener("click", takeTurn);
+var chooseYourGameBtns = document.querySelector(".choose-your-game-buttons");
+var classicModeBtn = document.getElementById("classicModeBtn");
+var difficultModeBtn = document.getElementById("difficultModeBtn");
+
+// Classic Mode Fighter Icons
+var rockBtnClassic = document.getElementById("rockBtnClassic");
+var paperBtnClassic = document.getElementById("paperBtnClassic");
+var scissorsBtnClassic = document.getElementById("scissorsBtnClassic");
+
+// Instantiating Player and Game Classes on (Load?)
+var humanPlayer = new Player ("Human", "🚀");
+var computerPlayer = new Player ("Computer", "👾");
+var newGame = new Game();
+
+//Event Listeners for Game Mode Selection Buttons
+// chooseYourGameBtns.addEventListener("click", newGame.selectGameType);
+classicModeBtn.addEventListener("click", function() {newGame.selectGameType("classic")});
+difficultModeBtn.addEventListener("click", function() {newGame.selectGameType("difficult")});
+
+//Event Listeners for Fighter Icons
+// rockBtnClassic.addEventListener("click", takeTurn);
+// paperBtnClassic = addEventListener("click", takeTurn);
+// paperBtnClassic = addEventListener("click", takeTurn);
+
+
+// Show/Hide by Class THESE DON'T WORK!!!!!!!
+function show(element) {
+  element.classList.add("hidden");
+}
+
+function hide(element) {
+  element.classList.remove("hidden");
+}
