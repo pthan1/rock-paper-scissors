80c9866d7e1090495451d71b93b7cbc530526c79
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-08-09 11:24:59 -0600

    Show respective game board on click

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
-var Player = require(".player.js");
+// var Player = require("./player.js");
 
 class Game {
   trackGameData() {
 
   }
 
-  selectGameType() {
-
+  selectGameType(type) {
+    if (type === "classic") {
+      chooseYourGameView.classList.add("hidden");
+      classicModeView.classList.remove("hidden");
+    }
+    
+    if (type === "difficult") {
+      chooseYourGameView.classList.add("hidden");
+      difficultModeView.classList.remove("hidden");
+    }
   }
 
   checkForWinConditions() {
